b8366c9baaae8c674068d74f3f1a444b7ec89c62
Thomas Brown <37277083+TCdev@users.noreply.github.com>
2018-12-03 22:15:14 -0800

    Add portfolio images, update canvas, css changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,71 @@
+/*
+  -------------------------
+    S N O W   F L A K E S
+  -------------------------
+*/
+window.onload = function() {
+
+  // GET CANVAS/CONTECT - STORED IN VAR
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+
+  $(document).mousemove(function(e){
+    $('.camvas').css({left:e.pageX, top:e.pageY});
+  })
+
+  // SET DIMENSIONS
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight * 0.4;
+
+  // SNOWFLAKE GENERATION
+  var maxFlakes = 75;
+  var flakes= [];
+
+  // APPLY ATTRIBUTES TO SNOW FLAKES
+  for(var i = 0; i < maxFlakes; i++) {
+    flakes.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      r: Math.random() * 5+1.5, // MIN OF 2px/MAX OF 6.5px - RADIUS
+      d: Math.random() + 1 // FLAKE DENSITY
+    })
+  }
+
+  // DRAW FLAKES ONTO CANVAS
+  function snowFlakes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.beginPath();
+    for(var i = 0; i < maxFlakes; i++) {
+      var f = flakes[i];
+      ctx.moveTo(f.x, f.y)
+      ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
+    }
+    ctx.fill();
+    dropFlakes();
+  }
+
+  // ANIMATE FLAKES
+  var angle = 0;
+  function dropFlakes() {
+    angle += 0.01;
+    for (var i = 0; i < maxFlakes; i++) {
+      // STORE CURRENT FLAKE
+      var f = flakes[i];
+
+      // UPDATE COORDINATES 
+      f.y += Math.pow(f.d, 2) + 1;
+      f.x += Math.sin(angle) + 2;
+
+      // START NEW SLOWFLAKE ONCE REACH BOTTOM
+      if(f.y > canvas.height) {
+        flakes[i] = {x: Math.random() * canvas.width, y: 0, r: f.r, d: f.d}
+      }
+    }
+  }
+
+  setInterval(snowFlakes, 25);
+}
 
 /*
   -----------------------
@@ -71,91 +139,3 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 };
-
-/*
-  -------------------------------
-    F L O A T I N G   L I N E S
-  -------------------------------
-*/
-
-var space;
-
-function floatySpace() {
-  var colors = [
-    "#FF3F8E", "#04C2C9", "#2E55C1"
-  ];
-
-
-  space = new CanvasSpace("canvas", "#252934" ).display();
-  var form = new Form( space );
-
-  // Elements
-  var pts = [];
-  var center = space.size.$divide(1.8);
-  var angle = -(window.innerWidth * 0.5);
-  var count = window.innerWidth * 0.05;
-  if (count > 150) count = 150;
-  var line = new Line(0, angle).to(space.size.x, 0);
-  var mouse = center.clone();
-
-  var r = Math.min(space.size.x, space.size.y) * 1;
-  for (var i=0; i<count; i++) {
-    var p = new Vector( Math.random()*r-Math.random()*r, Math.random()*r-Math.random()*r );
-    p.moveBy( center ).rotate2D( i*Math.PI/count, center);
-    p.brightness = 0.1
-    pts.push( p );
-  }
-
-  // Canvas
-  space.add({
-    animate: function(time, fps, context) {
-
-      for (var i=0; i<pts.length; i++) {
-        // rotate the points slowly
-        var pt = pts[i];
-
-        pt.rotate2D( Const.one_degree / 20, center);
-        form.stroke( false ).fill( colors[i % 3] ).point(pt, 1);
-
-        // get line from pt to the mouse line
-        var ln = new Line( pt ).to( line.getPerpendicularFromPoint(pt));
-
-        // opacity of line derived from distance to the line
-        var opacity = Math.min( 0.8, 1 - Math.abs( line.getDistanceFromPoint(pt)) / r);
-        var distFromMouse = Math.abs(ln.getDistanceFromPoint(mouse))
-
-        if (distFromMouse < 50) {
-          if (pts[i].brightness < 0.3) pts[i].brightness += 0.015
-        } else {
-          if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01
-        }
-
-        var color = "rgba(255,255,255," + pts[i].brightness +")"
-        form.stroke(color).fill( true ).line(ln);
-      }
-    },
-
-    onMouseAction: function(type, x, y, evt) {
-      if (type=="move") {
-        mouse.set(x,y);
-      }
-    },
-
-    onTouchAction: function(type, x, y, evt) {
-      this.onMouseAction(type, x, y);
-    }
-  });
-
-  space.bindMouse();
-  space.play();
-}
-
-floatySpace();
-
-$(window).resize(function(){
-  space.removeAll();
-  $('canvas').remove();
-  floatySpace();
-});
-
-
